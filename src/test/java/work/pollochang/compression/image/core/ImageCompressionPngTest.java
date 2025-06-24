d6bda17243ab45eaedd099a9dffb916e43b3d248
PolloChang <james89926282@gmail.com>
2025-06-24 23:02:59 +0800

    fix: remove

--- src/test/java/work/pollochang/compression/image/core/ImageCompressionPngTest.java
+++ src/test/java/work/pollochang/compression/image/core/ImageCompressionPngTest.java
@@ -1,6 +1,5 @@
 package work.pollochang.compression.image.core;
 
-import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 import work.pollochang.compression.image.report.CompressionParams;
